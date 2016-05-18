d316078732fcc0e734d8793c6077e97903ab89a7
Kiel Lofstrand <klofstrand@spscom.com>
2016-05-18 16:22:11 -0400

    Assembler works in 2 passes to compute symbol addresses

--- projects/06/assembler/pyasm/pyasm.py
+++ projects/06/assembler/pyasm/pyasm.py
@@ -133,22 +133,80 @@ class Code:
     def jump(self, mnem):
         return self.jumps[mnem]
 
+class SymbolTable:
+    symbols = {
+        "SP":     "0",
+        "LCL":    "1",
+        "ARG":    "2",
+        "THIS":   "3",
+        "THAT":   "4",
+        "R0":     "0",
+        "R1":     "1",
+        "R2":     "2",
+        "R3":     "3",
+        "R4":     "4",
+        "R5":     "5",
+        "R6":     "6",
+        "R7":     "7",
+        "R8":     "8",
+        "R9":     "9",
+        "R10":    "10",
+        "R11":    "11",
+        "R12":    "12",
+        "R13":    "13",
+        "R14":    "14",
+        "R15":    "15",
+        "SCREEN": "16384",
+        "KBD":    "24576",
+    }
+
+    def add(self, symb, addr):
+        self.symbols[symb] = addr
+
+    def contains(self, symb):
+        return symb in self.symbols
+
+    def address(self, symb):
+        return self.symbols[symb]
+
 if __name__ == "__main__":
     for arg in sys.argv[1:]:
         in_name = arg
         out_name = arg.replace(".asm", ".hack")
         print("Assembling %s -> %s..." % (in_name, out_name))
-        with open(in_name, "rt") as infd:
+        with open(out_name, "wt") as outfd:
             p = Parser()
             c = Code()
-            with open(out_name, "wt") as outfd:
+            s = SymbolTable()
+            print("   First pass...")
+            pc = 0
+            with open(in_name, "rt") as infd:
                 for text in infd:
                     p.advance(text)
                     if p.command_type:
                         if p.command_type == p.A_COMMAND:
-                            outfd.write("0{:015b}\n".format(int(p.symbol())))
+                            pc = pc + 1
                         elif p.command_type == p.L_COMMAND:
-                            print("(%s)" % p.symbol())
+                            s.add(p.symbol(), pc)
+                        elif p.command_type == p.C_COMMAND:
+                            pc = pc + 1
+            print("   Second pass...")
+            var = 16
+            with open(in_name, "rt") as infd:
+                for text in infd:
+                    p.advance(text)
+                    if p.command_type:
+                        if p.command_type == p.A_COMMAND:
+                            try:
+                                addr = int(p.symbol())
+                            except ValueError:
+                                if (s.contains(p.symbol())):
+                                    addr = int(s.address(p.symbol()))
+                                else:
+                                    addr = var
+                                    var = var + 1
+                                    s.add(p.symbol(), addr)
+                            outfd.write("0{:015b}\n".format(addr))
                         elif p.command_type == p.C_COMMAND:
                             outfd.write("111%s%s%s\n" % (c.comp(p.comp()), c.dest(p.dest()), c.jump(p.jump())))
-                        
+        print("   Done!")
